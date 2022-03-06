63f58894f468cadcf8359692d96bd74731e3e201
dytjq0123 <dytjq0123@gmail.com>
2022-03-06 20:15:20 +0900

    게시글 페이징 처리

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -3,6 +3,10 @@ package com.study.board.controller;
 import com.study.board.entity.Board;
 import com.study.board.service.BoardSerivce;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -50,9 +54,17 @@ public class BoardController {
     }
 
     @GetMapping("/board/list")
-    public String boardList(Model model){
-        List<Board> boardList = boardSerivce.boardList();
+    public String boardList(Model model, @PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable){
+        Page<Board> boardList = boardSerivce.boardList(pageable);
+        int nowPage = boardList.getPageable().getPageNumber() + 1;
+        int startPage = Math.max(nowPage - 4, 1);
+        int endPage = Math.min(nowPage + 5, boardList.getTotalPages());
+
         model.addAttribute("list", boardList);
+        model.addAttribute("nowPage", nowPage);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+
         return "boardList";
     }
 
